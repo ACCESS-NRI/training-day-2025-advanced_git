3536fb23244fad5933603e551df02969abdc0a32
Micael Oliveira <micael.oliveira@anu.edu.au>
2025-09-03 09:17:24 +1000

    Remove unnecessary comment.

--- package.py
+++ package.py
@@ -14,7 +14,11 @@ class AccessOm2(BundlePackage):
 
     git = "https://github.com/ACCESS-NRI/ACCESS-OM2.git"
 
+<<<<<<< HEAD
     maintainers("harshula")
+=======
+    maintainers("micael")
+>>>>>>> 7903a54 (Use maintainers function instead of list. Update maintainer.)
 
     version("latest")
 
@@ -27,4 +31,3 @@ class AccessOm2(BundlePackage):
     depends_on("mom5+deterministic", when="+deterministic", type="run")
     depends_on("mom5~deterministic", when="~deterministic", type="run")
 
-    # There is no need for install() since there is no code.
